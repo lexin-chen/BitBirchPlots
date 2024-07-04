91505e4bd0195adb71203b6590bde256314429ce
lexin-chen <lxchen017@gmail.com>
2024-07-03 21:37:14 -0400

    changed times format

--- 4-benchmark/times.py
+++ 4-benchmark/times.py
@@ -4,17 +4,16 @@ from matplotlib.ticker import FuncFormatter
 
 # Custom formatter function
 def millions_formatter(x, pos):
-    val = f'${{{x * 1e-6:.1f}}} \\cdot 10^6$'
+    val = f'{x/1e5:.0f}'
     return val
 plt.rcParams.update({'font.size': 12, 'font.weight': 'bold'})
 brc_times = np.loadtxt('../../data/pubchem_birch_timing.csv', delimiter=',', skiprows=1, usecols=(0, 1))
 but_times = np.loadtxt('../../data/pubchem_but_time_1.csv', delimiter=',', skiprows=1, usecols=(0, 1))
 
-
-plt.plot(but_times[:, 0], but_times[:, 1], label='Taylor-Butina', linewidth=5, linestyle='--',
+plt.plot(but_times[:, 0], but_times[:, 1], label='Taylor-Butina', linewidth=2, linestyle='--',
          c='#005cde')
-plt.plot(brc_times[:, 0], brc_times[:, 1], label='BitBirch', linewidth=5, c='#f99200')
-plt.xlabel('Number of molecules', fontsize=14, fontweight='bold')
+plt.plot(brc_times[:, 0], brc_times[:, 1], label='BitBirch', linewidth=2, c='#f99200')
+plt.xlabel(f'Number of molecules ($\\times 10^5$)', fontsize=14, fontweight='bold')
 plt.ylabel('Time (s)', fontsize=14, fontweight='bold')
 # Apply the formatter
 plt.gca().xaxis.set_major_formatter(FuncFormatter(millions_formatter))
